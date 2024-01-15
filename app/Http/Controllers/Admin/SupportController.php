e543215d01d1de32e01267fc08f6b67b07db698f
Aurélio Soares <asc.mat@gmail.com>
2024-01-15 20:08:05 -0300

    created support's model and migration / wrote support's controller

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
-    {
-        return view('admin/supports/index');
+    public function index(Support $support)
+    {        
+        $supports = $support->all();
+        
+        dd($supports);
+        return view('admin/supports/index', compact('supports')); // compact('supports) == ['supports' => $supports]
     }
 }
