154076f3b45db4478232bc99e72a4b8fe8c07604
Aurélio Soares <asc.mat@gmail.com>
2024-01-19 17:05:25 -0300

    adds field validation

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,14 @@
 <h1>New Doubt</h1>
+{{-- $errors is an automaticaly inject variable in all views --}}
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        {{ $error }}    
+    @endforeach
+@endif
 
 <form action="{{ route('supports.store') }}" method="POST">
     @csrf()
-    <input type="text" placeholder="Subject" name="subject">
-    <textarea name="body" cols="30" rows="5" placeholder="Description"></textarea>
+    <input type="text" placeholder="Subject" name="subject" value="{{ old('subject') }}">
+    <textarea name="body" cols="30" rows="5" placeholder="Description">{{ old('body') }}</textarea>
     <button type="submit">Send</button>
 </form>
